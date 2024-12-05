36471c476822c5b654421069fe83541a0d0a2c97
poco <poco@example.com>
2024-12-05 11:03:55 +0200

    update

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import pyautogui, time
-
-region = (700, 870, 400, 150)
+# (700, 870, 330, 150)
+region = (900, 1000, 100, 10)
 target_color = (83, 83, 83)
 
 time.sleep(1)
 while True:
     screenshot = pyautogui.screenshot(region=region)
-    screenshot.save("screenshot_region.png")
+    # screenshot.save("screenshot_region.png")
 
     width, height = screenshot.size
     found = False
-    for x in range(width):
+    for x in reversed(range(width)):
         for y in range(height):
             pixel_color = screenshot.getpixel((x, y))
             if pixel_color == target_color:
@@ -19,5 +19,5 @@ while True:
                 break
         if found:
             break
-    time.sleep(0.1)
+    time.sleep(0.05)
 
